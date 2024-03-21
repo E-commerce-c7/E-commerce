c0c241af6f47ffa595eb8dbadf9810c369ceb8ad
Youssefabdellatif33 <r.7xell@gmail.com>
2024-03-21 13:39:19 +0100

    66%

--- client/client/src/index.jsx
+++ client/client/src/index.jsx
@@ -15,7 +15,7 @@ import SingUp from "./components/singUp.jsx";
 
 const App = () => {
   const [view, setView] = useState('login');
-  const [product,setProduct] = useState(dummyData)
+  const [product,setProduct] = useState([])
   const [OnePrduct,setOneProduct] = useState({})
   const [key,setKey] = useState('')
  
@@ -44,72 +44,6 @@ const App = () => {
     })
   }
 
-  const dummyData = [
-    {
-      "id": 1,
-      "photo": "https://demo.evershop.io/assets/catalog/8247/2126/plv1439-White-list.png",
-      "name": "White Product 1",
-      "price": 19.99,
-      "sizes": ["M", "L", "XL"],
-      "colors": ["White", "Black"]
-    },
-    {
-      "id": 2,
-      "photo": "https://demo.evershop.io/assets/catalog/7659/2021/plv1120-White-list.png",
-      "name": "White Product 2",
-      "price": 24.99,
-      "sizes": ["S", "M", "L"],
-      "colors": ["White"]
-    },
-    {
-      "id": 3,
-      "photo": "https://demo.evershop.io/assets/catalog/1441/5452/plv4117-Blue-list.png",
-      "name": "Blue Product 1",
-      "price": 14.99,
-      "sizes": ["S", "M", "L"],
-      "colors": ["Blue"]
-    },
-    {
-      "id": 4,
-      "photo": "https://demo.evershop.io/assets/catalog/7599/6547/plv3643-Blue-list.png",
-      "name": "Blue Product 2",
-      "price": 29.99,
-      "sizes": ["M", "L"],
-      "colors": ["Blue", "Black"]
-    },
-    {
-      "id": 5,
-      "photo": "https://demo.evershop.io/assets/catalog/1034/3600/plv7632-Green-list.png",
-      "name": "Green Product",
-      "price": 9.99,
-      "sizes": ["S", "M"],
-      "colors": ["Green"]
-    },
-    {
-      "id": 6,
-      "photo": "https://demo.evershop.io/assets/catalog/5800/1660/plv5407-Black-list.png",
-      "name": "Black Product 1",
-      "price": 34.99,
-      "sizes": ["M", "L"],
-      "colors": ["Black"]
-    },
-    {
-      "id": 7,
-      "photo": "https://demo.evershop.io/assets/catalog/4477/5876/plv3335-Pink-list.png",
-      "name": "Pink Product",
-      "price": 19.99,
-      "sizes": ["M", "L"],
-      "colors": ["Pink"]
-    },
-    {
-      "id": 8,
-      "photo": "https://demo.evershop.io/assets/catalog/5761/1580/plv5236-Black-list.png",
-      "name": "Black Product 2",
-      "price": 39.99,
-      "sizes": ["S", "M", "L"],
-      "colors": ["Black"]
-    }
-  ];
 
 
   return (
